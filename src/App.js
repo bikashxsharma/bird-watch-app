4e9a90e16b076161bf2878e6054596354b4d3003
Bikash Sharma <36207673+bikashxsharma@users.noreply.github.com>
2020-01-31 14:32:58 +0200

    11

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'; 
+import FormDataComponent from './components/form-data.component';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+
+      
+      {/* https://dev.to/chaituknag/a-simple-journal-app-using-react-localstorage-and-fun-23j8 */}
+     
     </div>
   );
 }
