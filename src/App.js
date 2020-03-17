d0017a3f8844fc22cfcb97970c357bf8d2f42743
Bikash Sharma-home-pc <bikashxsharma@gmail.com>
2020-03-17 23:20:23 +0200

    new-version-from-scratch

--- src/App.js
+++ src/App.js
@@ -1,184 +1,122 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react'
 import './App.css';
 import moment from 'moment';
 
-const {useState, useEffect, useRef} = React;
+const App = () => {
 
-function useJournal() {
-  const [entries, setEntries] = useState([]);
-  
-  const getEntriesFromStorage = () => JSON.parse(
-    window.localStorage.getItem('birdEntries')
+  const [name, setName] = useState('');
+  const [info, setInfo] = useState('');
+  const [rarity, setRarity] = useState('');
+
+  const [birdDatas, setBirdDatas] = useState([]);
+
+  // get data from storage 
+  const getDataFromStorage = () => JSON.parse(
+    window.localStorage.getItem('birdDatas')
   );
-  const setEntriesToStorage = items => 
-  window.localStorage.setItem('birdEntries', JSON.stringify(items));
+
+
+
+  // store data to local storage
+  const setDataToStorage = (items) => window.localStorage.setItem('birdDatas', JSON.stringify(items));
+
+  // check for storage data and updates birdDatas  
   useEffect(() => {
-    const entriesFromStorage = getEntriesFromStorage();
-    if(entriesFromStorage) {
-      setEntries(entriesFromStorage);
+    const entriesFromStorage = getDataFromStorage();
+    console.log(entriesFromStorage);
+    if (entriesFromStorage) {
+      setBirdDatas(entriesFromStorage);
     }
-  }, []);
-  
-  const storeEntry = (entry) => {
-    const newEntries = [entry, ...entries];
-    setEntries(newEntries);
-    setEntriesToStorage(newEntries);
-  }
-  
-  const removeEntry = (index) => {
-    const newEntries = [...entries.slice(0, index), ...entries.slice(index+1)];
-    setEntries(newEntries);
-    setEntriesToStorage(newEntries);
-  }
-  
-  return [entries, storeEntry, removeEntry];
-}
+  }, [])
 
-function EntryList({list, deleteEntry}) {
-  const handleDeleteClick = (index) => e => {
-    deleteEntry(index);
-  }
-  return (
-    <div className="entry-list mt-3">
-      {
-        list && list.map((item, i) => {
-          const itemDate = moment(item.date).fromNow();
-          const flagColor = item.flag ? `bg-${item.flag} text-white` : '';
-          return (
-            <div className={`card mb-2 ${flagColor}`}>
-              <div className="card-body">
-                <h4 className="card-title">{itemDate}</h4>
-                <p className="card-text">Species name : {item.birdName}</p>
-                <p className="card-text">{item.noteInfo}</p>                
-                <p className="card-text">Rarity : {item.flag}</p>                
-                <p><img src={item.imageFile} /></p>
-                <button className="btn btn-sm btn-exRare" 
-                  onClick={handleDeleteClick(i)}>Delete</button>
-              </div>
-            </div>  
-          )
-        })
-      }
-    </div>
-  )
-}
+  // function to store data to local storage
 
-function Entry({addEntry}) {
-  const [noteInfo, setnoteInfo] = useState('');
-  const [birdName, setBirdName]= useState('');
-  const [imageFile, uploadImage] = useState('');
-  const [flag, setFlag] = useState('');
-  const fieldRef = useRef();
-  const handleOnChange = e => setnoteInfo(e.target.value);
-  const handleOnChangeName = e =>setBirdName(e.target.value);
-  const handleFlagChange = e => setFlag(e.target.value);
-  const handleOnChangeImage = e => uploadImage(e.target.value);
-  const handleOnSubmit = e => {
+  const storeData = (data) => {
+    const newData = [data, ...birdDatas];
+    setBirdDatas(newData);
+    setDataToStorage(newData);
+
+  }
+
+  const updateName = (e) => {
+    setName(e.target.value);
+  }
+  const updateInfo = (e) => {
+    setInfo(e.target.value);
+  }
+  const updateRarity = (e) => {
+    setRarity(e.target.value);
+  }
+
+  const handleSubmit = (e) => {
     e.preventDefault();
-    if(noteInfo && noteInfo.trim().length > 0) {
-      addEntry({
-        noteInfo,
-        birdName,
-        imageFile,
-        flag,
-        date: Date.now()
-      });
-      setnoteInfo('');
-      setBirdName('');
-      uploadImage('');
-      setFlag('');
+    const data = {
+      name: name,
+      info: info,
+      rarity: rarity,
+      date: Date.now()
     }
+    storeData(data);
+
   }
-  
-  useEffect(() => {
-    fieldRef.current.focus();
-  }, []);
-  
+
   return (
-    <form onSubmit={handleOnSubmit}>
-      <div className="form-group">
-      <label htmlFor="noteInfo">Enter the bird information:</label>
-      <textarea 
-        className="form-control"
-        value={noteInfo} 
-        onChange={handleOnChange} 
-        type="text" 
-        id="noteInfo" 
-        maxLength={100}
-        ref={fieldRef}
-        />
+    <div className="App">
+      <h2>Bird Watch</h2>
+      <form className="form" onSubmit={handleSubmit}>
+        <h3>Enter bird Information</h3>
+        <div className="form-group">
+          <label>Name</label>
+          <input type="text" placeholder="Species name.." onChange={updateName} />
+        </div>
+        <div className="form-group">
+          <label>Info</label>
+          <textarea placeholder="Add extra information.." onChange={updateInfo}></textarea>
+        </div>
+        <div className="form-group">
+          <label> Select the rarity type:</label>
+          <div className="radio-options">
+            <div className="radio-input"> <input type="radio" name="rarity" value="common" onChange={updateRarity} /> Common</div>
+            <div className="radio-input"><input type="radio" name="rarity" value="rare" onChange={updateRarity} /> Rare</div>
+            <div className="radio-input"><input type="radio" name="rarity" value="extremely rare" onChange={updateRarity} /> Extremely rare</div>
+          </div>
+          <div className="form-buttons">
+            <input type="submit" className="button" value="Save" />
+            <input type="reset" className="button" value="Cancel" />
+          </div>
+
+        </div>
+      </form>
+
+      <div className="bird-info-results">
+        {
+
+          birdDatas.map(data => (
+
+
+            <div className="bird-card">
+              <p>{moment(data.date).fromNow()}</p>
+              <p>{data.name}</p>
+              <p>{data.info}</p>
+              <p>{data.rarity}</p>
+              <hr />
+
+            </div>
+
+          ))
+        }
+
       </div>
 
-      <div className="form-group">
-      <label htmlFor="birdName">Enter the bird species:</label>
-      <input
-        className="form-control"
-        value={birdName} 
-        onChange={handleOnChangeName} 
-        type="text" 
-        id="birdName" 
-        maxLength={40}
-        />
-      </div>
-      <div className="form-group">
-      <label htmlFor="birdName">Upload your image</label>
-      <input
-        className="form-control"
-        onChange={handleOnChangeImage} 
-        type="text" 
-        id="imageFile"
-        name="imageFile" 
-        />
-      </div>
-      
 
-      <div class="form-check form-check-inline mb-3">
-        <label>Select the rarity</label>
-        <input className="form-check-input" 
-          id="exRare" type="radio" name="rarity" 
-          value="exRare" defaultChecked={flag === 'exRare'} 
-          onChange={handleFlagChange}/> 
-        <label className="form-check-label bg-exRare " 
-          htmlFor="exRare">Extremely rare</label>
 
-        <input className="form-check-input" 
-          id="rare" type="radio" name="rarity" 
-          value="rare" defaultChecked={flag === 'rare'} 
-          onChange={handleFlagChange}/>
-        <label className="form-check-label bg-rare " 
-          htmlFor="rare">Rare</label>
 
-        <input className="form-check-input" 
-          id="common" type="radio" name="rarity" 
-          value="common" defaultChecked={flag === 'common'} 
-          onChange={handleFlagChange}/>
-        <label className="form-check-label bg-common " 
-          htmlFor="common">Common</label>
-      </div>
 
-      <button  
-        className="form-control" 
-        type="cancel">Cancel</button>
-        <button disabled={noteInfo.trim().length === 0 || !flag} 
-        className="btn btn-success form-control" 
-        type="submit">Save</button>
-    </form>
-  );
-}
 
-function App() {
-  const [entries, storeEntry, removeEntry] = useJournal();
-  const handleAddEntry = (entry) => storeEntry(entry);
-  const handleDeleteEntry = (index) => removeEntry(index);
-  return (
-    <div className="container">
-      <h1 className="text-center">
-        Bird watch:
-      </h1>
-      <Entry addEntry={handleAddEntry}/>
-      <EntryList list={entries} deleteEntry={handleDeleteEntry}/>
+
     </div>
   )
-}
 
+
+}
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './custom.css';
-
 
 import App from './App';
 
